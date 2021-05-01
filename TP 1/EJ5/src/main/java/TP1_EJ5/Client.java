3441236ddda1e5316185c382507bdcddae5678b5
jbert <joaquin.bert@agroconsultasonline.com.ar>
2021-05-01 00:38:46 -0300

    Ej 5

---
+++ TP 1/EJ5/src/main/java/TP1_EJ5/Client.java
@@ -0,0 +1,36 @@
+package TP1_EJ5;
+
+
+import com.google.gson.Gson;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Client {
+    Gson gson = new Gson();
+
+    public Client (int port){
+        try{
+            Registry clientRMI = LocateRegistry.getRegistry("127.0.0.1", port);
+            String[] servicesList = clientRMI.list();
+
+            for (String service : servicesList){
+                System.out.println(" Service: " + service);
+            }
+            RemoteInt ri = (RemoteInt) clientRMI.lookup("WeatherServer");
+            String weather = ri.getWeather("BSAS");
+            System.out.println("WeatherServer: " + weather);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public static void main( String[] args )
+    {
+        // parametros de consola
+        int port = 9090;
+        Client client = new Client(port);
+    }
+}
